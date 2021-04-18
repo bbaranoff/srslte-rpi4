c1012ba15c8a7f6a48661bc7a4f9b534654a14ca
bbaranoff <37385191+bbaranoff@users.noreply.github.com>
2021-04-18 20:58:11 +0200

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
+!!!!!!!!!!!!!!!!! Procédure technique d'installation !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
 sudo su
 mkdir /opt/GSM
 cd /opt/GSM
@@ -25,7 +29,6 @@ cd /opt/GSM
 echo "export  UHD_MODULE_PATH=/usr/lib/uhd/modules" > /root/.bashrc
 
 sudo apt install cpufrequtils
-sudo cpufreq-set -c 0-3 -g performance -u 1500000
 
 sudo apt install libsctp-dev libconfig++-dev libconfig-dev libmbedtls-dev
 git clone https://github.com/srsLTE/srsLTE
@@ -36,9 +39,34 @@ wget https://github.com/bbaranoff/srslte-rpi4/blob/main/epc.conf
 wget https://github.com/bbaranoff/srslte-rpi4/blob/main/enb.conf
 cp enb.conf /root/.config/srslte/enb.conf
 cp epc.conf /root/.config/srslte/epc.conf
+
+editer le fichier user_db.csv
 nano /root/.config/user_db.csv
 ue1,mil,!!!!!!!!!!!!CHANGER PAR VALEUR IMSI FOURNIE PAR SYSMOCOM!!!!!!!!!!!!!,!!!!!!!!!!!!!!!!!!!CHANGER PAR Ki FOURNIE PAR SYSMOCOM!!!!!!!!!!!!!!!!,opc,!!!!!!!!!!!!!!!CHANGER PAR VALEUR OPC FOURNIE PAR SYSMOCOM!!!!!!!!!!!!!!!!!,9001,000000001234,7,dynamic
 
+
+!!!!!!Attentions le MNC et MCC de la SIM doivent etre de 208 et 92 sinon changer dans enb.conf et epc.conf!!!!!!!!!!!
+
+!!!!!!!!!!!!!!!!!!Procédure technique d'éxecution !!!!!!!!!!!!!!!!!!!!
+
+
 srsepc_if_masq.sh eth0
+sudo cpufreq-set -c 0-3 -g performance -u 1500000
+
+en tant que root
+
+shell #1
+srsepc
+
+shell #2
+srsenb
+
+LES FICHIER A RECUPERER SE TROUVENT DANS
+
+[pcap]
+enable = true
+filename = /tmp/enb.pcap
+s1ap_enable = true
+s1ap_filename = /tmp/enb_s1ap.pcap
 
 
